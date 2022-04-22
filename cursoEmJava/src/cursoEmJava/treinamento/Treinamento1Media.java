f6c379f009d2b5a69c01c98446f645271e896d0c
DevJava <DevJava@DESKTOP-DEV8RBN>
2022-04-22 06:36:13 -0300

    Atualizando if verificando nome do aluno

--- cursoEmJava/src/cursoEmJava/treinamento/Treinamento1Media.java
+++ cursoEmJava/src/cursoEmJava/treinamento/Treinamento1Media.java
@@ -3,10 +3,10 @@ package cursoEmJava.treinamento;
 public class Treinamento1Media {
 	public static void main(String[] args) {
 		//testando 
-		double nota1 = 10;
-		double nota2 = 10;
-		double nota3 = 10;
-		double nota4 = 10;
+		double nota1 = 5;
+		double nota2 = 6;
+		double nota3 = 6;
+		double nota4 = 6;
 		double mediaTotal = 0;
 		String nomeAluno = "Rafael Morales";
 		mediaTotal = (nota1 + nota2 + nota3 + nota4 )/4;
@@ -16,17 +16,17 @@ public class Treinamento1Media {
 		System.out.println("Aluno terceira nota : " +nota3);
 		System.out.println("Aluno quarta nota : " +nota4 );
 		
-		if(mediaTotal >= 7) {
+		if(mediaTotal >= 7 && nomeAluno.equals("Rafael Morales")) {
 			System.out.println("Parabéns");
 			System.out.println(nomeAluno+ " sua média é : " +mediaTotal+ " você esta aprovado");
 			
-		}else if(mediaTotal >=6) {
+		}else if(mediaTotal >=6 && nomeAluno.equals("Rafael Morales")) {
 			System.out.println("Ótimo");
-			System.out.println(nomeAluno+ " sua média é : " +mediaTotal+ "você esta de exame");
+			System.out.println(nomeAluno+ " sua média é : " +mediaTotal+ " você esta de exame");
 			
 		}else {
 			System.out.println("Deu ruim");
-			System.out.println(nomeAluno+ " sua média é : " +mediaTotal+ "você esta reprovado ");
+			System.out.println(nomeAluno+ " sua média é : " +mediaTotal+ " você esta reprovado ");
 		}
 		
 	}

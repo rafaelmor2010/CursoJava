90ae313fc9255911a4c0ae65d675f204bbc98154
DevJava <DevJava@DESKTOP-DEV8RBN>
2022-04-23 22:37:55 -0300

    Algoritmo Repetição 10x

---
+++ cursoEmJava/src/cursoEmJava/treinamento/RepeticaoForAté10.java
@@ -0,0 +1,25 @@
+package cursoEmJava.treinamento;
+
+public class RepeticaoForAté10 {
+	public static void main(String[] args) {
+		
+		String nome = "Rafael Morales";
+		String curso = "Java";
+		
+		System.out.println("Programa Repetição 10X");
+		
+		if (curso.equals("Java")) {
+			
+			for(int numero = 0; numero <=10; numero++ ) {
+				System.out.println("Olá aluno " +nome+ " esta cursando " +curso+ "" +numero);
+				
+			}
+			
+		}else {
+			System.out.println("Olá " +nome+ " Você não esta cursando "+curso );
+		}
+		
+				
+	}
+
+}
